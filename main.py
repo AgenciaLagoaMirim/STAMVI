328cd88e28f70cdb535b3bb325cb6c88bf1f439d
Jamilson do Nascimento <iero.eng.agricola@gmail.com>
2023-01-21 20:29:41 -0300

    atualiza main.py

--- main.py
+++ main.py
@@ -1,102 +1,66 @@
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import ttk
 
-import matplotlib.backends.backend_tkagg
-import matplotlib.pyplot as plt
-import numpy as np
-import pandas as pd
-from sklearn.linear_model import LinearRegression
+from src.useCases import (
+    DatDataProcessing,
+    ExportData,
+    LeastSquareHeightArea,
+    XmlDataProcessing,
+)
 
-from src.useCases import DatDataProcessing, ExportData, XmlDataProcessing
 
-APP_WIDTH = 1292
-APP_HEIGHT = 380
+APP_HEIGHT = 550
+APP_WIDTH = 650
 
 
 class MainApp:
     def __init__(self):
+
+        # configuração janela principal
         self.root = tk.Tk()
-        self.root.state("zoomed")
-
-        self.container_frame = tk.Frame(self.root)
-        self.container_frame.place(relx=0, rely=0, relheight=1, relwidth=0.998)
-
+        self.root.geometry(f"{APP_HEIGHT}x{APP_WIDTH}")
+        self.root.resizable(0, 0)
         # use cases
         self.xml_data_processing = XmlDataProcessing()
         self.dat_data_processing = DatDataProcessing()
         self.export_data = ExportData()
+        self.least_square_height_area = LeastSquareHeightArea()
 
-        # LabelFrames
-        self.plot_frame_vel_avgVel = tk.LabelFrame(
-            self.root,
-            text="Plot Relation Velocity X (m/s) - Average Velocity(m/s) ",
-            border=1.5,
-            relief="solid",
-        )
-        self.plot_frame_vel_avgVel.place(
-            relx=0.53, rely=0, relheight=0.6, relwidth=0.45
-        )
+        # Frame da TreeViewWidget
+        self.trv_frame = tk.LabelFrame(self.root, text="Final DataFrame")
+        self.trv_frame.place(height=225, width=650)
 
-        self.plot_frame_height_area = tk.LabelFrame(
-            self.root,
-            text="Plot Relation Height (m) - Mean Area(m²) ",
-            border=1.5,
-            relief="solid",
-        )
-        self.plot_frame_height_area.place(
-            relx=0.005, rely=0, relheight=0.6, relwidth=0.45
-        )
+        # Frame Open File Dialog
+        self.file_dialog_frame = tk.LabelFrame(self.root, text="Files Dialog")
+        self.file_dialog_frame.place(height=100, width=400, rely=0.65, relx=0)
+
+        # Frame Botões
 
         self.btn = ttk.Button(
-            self.container_frame,
+            self.file_dialog_frame,
             padding=2,
             text="Botao",
-            command=self.plot_least_square_height_area(),
+            command=self.least_square_height_area.plot_least_squares(),
         )
+
+        # TreeViewWidget
+        self.tree_view = ttk.Treeview(self.trv_frame)
+        self.tree_view.place(relheight=1, relwidth=1)
+
+        self.trv_scroll_y = ttk.Scrollbar(
+            self.trv_frame, orient="vertical", command=self.tree_view.yview
+        )
+        self.trv_scroll_x = ttk.Scrollbar(
+            self.trv_frame, orient="horizontal", command=self.tree_view.xview
+        )
+
+        self.tree_view.configure(
+            xscrollcommand=self.trv_scroll_y.set, yscrollcommand=self.trv_scroll_x.set
+        )
+        self.trv_scroll_x.pack(side="bottom", fill="x")
+        self.trv_scroll_y.pack(side="right", fill="y")
 
         self.root.mainloop()
-
-    def plot_least_square_height_area(self):
-        file_path = filedialog.askopenfilename(filetypes=[("Excel Files", ".xlsx")])
-        xlsx_dataFrame = pd.read_excel(file_path)
-
-        height_list = np.array(xlsx_dataFrame.height_m)
-        height_mtx = height_list.reshape(-1, 1)
-
-        area_list = np.array(xlsx_dataFrame.mean_area_sqm)
-        # area_mtx = area_list.reshape(-1, 1)
-        # treina o modelo
-        linear_model = LinearRegression()
-        linear_model.fit(height_mtx, area_list)
-        # obtem previsões
-        area_predict = linear_model.predict(height_mtx)
-
-        # plota os dados e a linha de regressão
-        plt.scatter(height_list, area_list)
-        plt.plot(height_list, area_predict, color="red")
-
-        # adiciona a equação da linha de regressão
-        plt.annotate(
-            f"mean_area(m²) = {linear_model.coef_[0]:.2f}height(m) + {linear_model.intercept_:.2f}",
-            xy=(height_list.min(), area_predict.min()),
-            xytext=(height_list.min(), area_predict.max()),
-            # arrowprops=dict(arrowstyle="->", color="red"),
-        )
-
-        # adiciona as legandas
-        plt.xlabel("Height(m)")
-        plt.ylabel("Mean Area (m²)")
-
-        # adiciona o valor de R²
-        plt.title(f"R² = {linear_model.score(height_mtx, area_list):.4f}")
-
-        # exibe o grafico
-
-        canvas = matplotlib.backends.backend_tkagg.FigureCanvasTkAgg(
-            plt.gcf(), master=self.plot_frame_height_area
-        )
-        canvas.draw()
-        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
 
 if __name__ == "__main__":
--- src/useCases/__init__.py
+++ src/useCases/__init__.py
@@ -3,3 +3,4 @@ from src.useCases.datDataProcessing import DatDataProcessing
 from src.useCases.exportData import ExportData
 from src.useCases.xlsxToDataFrame import XlsxToDataFrame
 from src.useCases.xmlDataProcessing import XmlDataProcessing
+from src.useCases.leastSquareHeightArea import LeastSquareHeightArea
