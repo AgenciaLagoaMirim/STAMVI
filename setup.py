11b042ca44792d6787c6efb631176474f6dbac3b
Jamilson do Nascimento <iero.eng.agricola@gmail.com>
2023-02-04 03:18:54 -0300

    adiciona setup.py

---
+++ setup.py
@@ -0,0 +1,28 @@
+import sys
+
+from cx_Freeze import Executable, setup
+
+# Read dependencies from requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+build_exe_options = {
+    "zip_include_packages": ["numpy", "pandas", "matplotlib", "scikit-learn"],
+    "include_files": ["requirements.txt"],
+}
+
+# Path to your main.py file.
+base = None
+if sys.platform == "win32":
+    base = "Win32GUI"
+
+executables = [Executable("main.py", base=base)]
+
+setup(
+    name="NomeDoSeuProjeto",
+    version="0.1",
+    description="Descrição do seu projeto",
+    options={"build_exe": build_exe_options},
+    executables=executables,
+    install_requires=requirements,
+)
